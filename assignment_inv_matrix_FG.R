87f90d8d6cef85128fa90a37b60e3c2e694054b2
fabiogiud <34208030+fabiogiud@users.noreply.github.com>
2017-12-22 11:41:27 +0100

    Add files via upload
    This file is an R file that calculates the inverse of a matrix and cache tha value

---
+++ assignment_inv_matrix_FG.R
@@ -0,0 +1,37 @@
+
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
+
+
+a <- matrix(c(1,5,6,7,9,3,11,5,6),3,3)
+a
+
+b <- makeCacheMatrix(a)
+c <- cacheSolve(b)
+a%*%c     # ok!
+d <- cacheSolve(b)
+d
